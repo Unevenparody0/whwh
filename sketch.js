2728153358dfb2c1478ca8a29b6675d077f1e5a3
Unevenparody0 <78752847+Unevenparody0@users.noreply.github.com>
2021-04-19 15:10:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,87 @@
+var starImg,bgImg;
+var star, starBody;
+var fairy,fairyimg;
+var sond;
+//create variable for fairy sprite and fairyImg
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	starImg = loadImage("star.png");
+	bgImg = loadImage("starNight.png");
+	//load animation for fairy here
+	fairyimg = loadAnimation("fairyImage1.png","fairyImage2.png");
+	sond = loadSound("JoyMusic.mp3");
+	
+}
+
+function setup() {
+	createCanvas(800, 750);
+
+	//write code to play fairyVoice sound
+
+	//create fairy sprite and add animation for fairy
+
+
+	star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
+	fairy = createSprite(120,550)
+    fairy.addAnimation("nicname",fairyimg);
+	fairy.scale = 0.3;
+    
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, starBody);
+	
+	Engine.run(engine);
+
+}
+
+
+function draw() {
+  background(bgImg);
+
+  star.x= starBody.position.x 
+  star.y= starBody.position.y 
+
+  console.log(star.y);
+  if(keyDown("right")){
+
+	fairy.x=fairy.x+10
+  }
+if(keyDown("left")){
+fairy.x=fairy.x-10
+}
+
+
+if(fairy.isTouching(star)){
+
+sond.play();
+star.y = 500;
+
+}
+
+  //write code to stop star in the hand of fairy
+
+  drawSprites();
+
+}
+
+function keyPressed() {
+
+	if (keyCode === DOWN_ARROW) {
+		Matter.Body.setStatic(starBody,false); 
+	}
+
+	//writw code to move fairy left and right
+	
+}
